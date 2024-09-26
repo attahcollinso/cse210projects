6b4a5c487e672032e519439e84cea2c6010c4836
attahcollinso <attahcollinso@gmail.com>
2024-09-26 10:30:48 +0100

    Finished abstraction

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Developer";
+        job1._company = "Secons2None Services";
+        job1._startYear = 2019;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Access Bank";
+        job2._startYear = 2012;
+        job2._endYear = 2020;
+
+        Resume jobResume = new Resume();
+        jobResume._name = "Attah collins";
+
+        jobResume._jobs.Add(job1);
+        jobResume._jobs.Add(job2);
+
+        jobResume.Display();
     }
 }
