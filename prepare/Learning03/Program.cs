375a32d5bf2510730e6f747eca5fd13a7af973f5
attahcollinso <attahcollinso@gmail.com>
2024-10-03 15:15:40 +0100

    week03 Fraction learning activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,27 @@
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction test = new Fraction(); 
+        Console.WriteLine(test.GetFractionString());
+        Console.WriteLine(test.GetDecimalValue());
+
+        Fraction test1 = new Fraction(5);
+        Console.WriteLine(test1.GetFractionString());
+        Console.WriteLine(test1.GetDecimalValue());
+
+        Fraction test2 = new Fraction(3, 4);
+        Console.WriteLine(test2.GetFractionString());
+        Console.WriteLine(test2.GetDecimalValue());
+
+        test2.SetTop(1);
+        test2.SetBottom(3);
+        Console.WriteLine(test2.GetFractionString());
+        Console.WriteLine(test2.GetDecimalValue());
+
     }
 }
+   
