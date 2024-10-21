45cbacee0ba7bc242115b8dc2c977facc38b7ba8
attahcollinso <attahcollinso@gmail.com>
2024-10-21 13:33:38 +0100

    week06 Learning activity

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Yellow", 5);
+        shapes.Add(square);
+
+        Rectangle rangle = new Rectangle("Blue", 4, 5);
+        shapes.Add(rangle);
+
+        Circle circle = new Circle("Green", 7);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
